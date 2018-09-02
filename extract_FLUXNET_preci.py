a054a4a38bda4b78e5e70a83d536235c5fe3fb36
Savannahmyself <yangleimyself@163.com>
2018-09-02 20:54:30 +0800

    Update extract_FLUXNET_preci.py

--- extract_FLUXNET_preci.py
+++ extract_FLUXNET_preci.py
@@ -1,26 +1,46 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from dateutil.parser import parse
 
-x = []
-y = []
-df = pd.read_csv(r'AR_Slu_data.csv', encoding='utf-8')
+def extract_preci(path):
+    x = []
+    y = []
+    df = pd.read_csv(path, encoding='utf-8')
+    #提取起始时间和降水数据
+    for i in df.TIMESTAMP_START:
+        x.append(i)
+    for i in df.P_F:
+        y.append(i)
 
-#提取起始时间数据
-for i in df.TIMESTAMP_START:
-    x.append(i)
-#提取降水数据
-for i in df.P_F:
-    y.append(i)
+    # 筛选雨量大于0的降水事件，并制作标签数组
+    label = []
+    value = []
+    i = 50
+    for x_, y_ in zip(x, y):
+        if y_:
+            if i % 50 == 0:
+                label.append(x_)
+            else:
+                label.append('')
+            value.append(y_)
+            i += 1
 
-y = [x for x in y if x>0]
+    # 绘图
+    plt.bar(range(len(value)), value, align='center', color='steelblue', alpha=0.8)
+    # 添加轴标签
+    plt.ylabel('precipitation')
+    # 添加标题
+    plt.title('Comparing about precipitation')
+    # 添加X轴范围
+    plt.xlim((-5, len(label) + 50))
+    # 设置Y轴的刻度范围
+    # plt.ylim()
+    # 添加X轴刻度标签
+    plt.xticks(range(len(label)), label, rotation=45)
+    plt.savefig("precipitation.png")
+    plt.show()
 
-# 绘图
-plt.bar(range(len(y)), y, align='center', color='steelblue', alpha=0.8)
-# 添加轴标签
-plt.ylabel('precipitation(mm)')
-# 添加标题
-plt.title('Comparing about precipitation')
-plt.savefig("precipitation.png")
-plt.show()
+if __name__ == '__main__':
+
+    filepath = r'AR_Slu_data.csv'
+    extract_preci(filepath)
 
